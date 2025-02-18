25c6892be6749ebc9adaa8c98e6f386e6417aeb2
MA Ahi <bijoyrashidul21@gmail.com>
2025-02-18 07:42:48 +0600

    small update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const socketIo = require("socket.io");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // CORS Setup
 const allowedOrigins = [
